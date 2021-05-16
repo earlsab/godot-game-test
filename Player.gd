02bd18ed058eae6559b059c7695c68d806e79f6b
Earlan Sabillano <earlanjoshqs@gmail.com>
2021-05-16 19:00:57 +0800

    initial
    broken code, commited to transfer to another system

--- Player.gd
+++ Player.gd
@@ -1,32 +1,25 @@
-extends Area2D
+extends KinematicBody2D
 
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
 
-export var speed = 400
-var screen_size
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite.play()
-	screen_size = get_viewport_rect().size
+    screen_size = get_viewport_rect().size
 
 func _process(delta):
-	var velocity = Vector2()
+	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-	if velocity.x == 0:
-		$AnimatedSprite.animation = "idle"
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "run"
-		$AnimatedSprite.flip_h = velocity.x < 0
+		$AnimatedSprite.play()
 	else:
-		pass
-		
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+		$AnimatedSprite.stop()
+
+		position += velocity * delta

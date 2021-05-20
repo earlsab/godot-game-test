bd5b1f43b7441969c56c3c1503c7fcf11910d304
Earlan Sabillano <earlanjoshqs@gmail.com>
2021-05-20 22:45:42 +0800

    implemented slow walk and run through moveState

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,24 +3,34 @@ extends KinematicBody2D
 const GRAVITY = 20 
 const RESISTANCE = Vector2(0, -1)
 
-export var RUN = 200
-export var WALK = 500
+export var SWALK = 100
+export var RUN = 500
+export var WALK = 200
 export var JUMP = -500
 
 # TODO: Find out the reason why calling for Vector2()
 # automatically changes movement
 var movement = Vector2()
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	movement.y += GRAVITY
-	
-	# Walk
+	var moveState
+
+	# Movement States
+	if Input.is_action_pressed("ui_shift"):
+		moveState = RUN
+	elif Input.is_action_pressed("ui_ctrl"):
+		moveState = SWALK
+	else:
+		moveState = WALK
+			
+	# Player Input
 	if Input.is_action_pressed("ui_right"):
-		movement.x = WALK
+		movement.x = moveState
 		$spr_player.flip_h = false
 		$spr_player.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		movement.x = -(WALK)
+		movement.x = -(moveState)
 		$spr_player.flip_h = true
 		$spr_player.play("Run")
 	else:

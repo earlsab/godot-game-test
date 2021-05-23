c752a5f543f8e05d8c484bf8273d0fdc0564d945
Earlan Sabillano <earlanjoshqs@gmail.com>
2021-05-23 23:14:04 +0800

    variable change

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,32 +3,32 @@ extends KinematicBody2D
 const GRAVITY = 20 
 const RESISTANCE = Vector2(0, -1)
 
-export var SWALK = 100
+export var CROUCH = 100
 export var RUN = 400
 export var WALK = 300
 export var JUMP = -500
  
-
 # TODO: Update Notion, Vector2 directly changes node's position
-var animationState = "Idle"
+# var animationState = "Idle"
 var movement = Vector2()
+var animationState
 
 func _physics_process(_delta):
 	movement.y += GRAVITY
 	var moveState
-	# var animationState
 
 	# Movement States
 	if Input.is_action_pressed("ui_extra1"):
 		moveState = RUN
 	elif Input.is_action_pressed("ui_down"):
-		moveState = SWALK
+		moveState = CROUCH 
 	else:
 		moveState = WALK
 
 	# Animation States
 	if is_on_floor():
-		if moveState == SWALK:
+		if moveState == CROUCH:
+			crouch()
 			if movement.x == 0:
 				animationState = "CrouchIdle"
 			else:
@@ -56,8 +56,10 @@ func _physics_process(_delta):
 		movement.x = 0
 	$spr_player.play(animationState)
 	
-	# Jump
 	if Input.is_action_pressed("ui_jump") and is_on_floor():
 		movement.y = JUMP
 
 	movement = move_and_slide(movement, RESISTANCE)
+
+func crouch():
+	animationState = "CrouchTransi"

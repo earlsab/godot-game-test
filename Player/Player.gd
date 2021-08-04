f90b562055650da119181a5065abed7925a57a35
earlsab <62688314+earlsab@users.noreply.github.com>
2021-08-04 18:28:11 +0800

    jank but working mouse click teleportation system

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,23 +9,31 @@ const JUMP_FORCE = 120
 
 var MOVE_ADJUST
 var motion = Vector2.ZERO
+var click_pos = Vector2()
 onready var bullet_time_filter = get_node("/root/Main/HUD/BulletTimeIndicator")
 onready var error_filter = get_node("/root/Main/HUD/ErrorIndicator")
 onready var spr_player = $spr_player
-onready var extra_input = {}
+onready var movement_input = {}
+onready var skill_input = {}
 onready var basic_animation_ignore = false
 onready var skill = false
 onready var gravity_ignore = false
 
-	
+func _unhandled_input(event):
+	if event.is_action_pressed('ui_click'):
+		click_pos = get_global_mouse_position()
+
+# func _process(delta):
+# 	pass
+
 func _physics_process(delta):
 	# Handles Important Input
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var y_input = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
 	# Handles Extra Input
-	extra_input.Crouch = true if Input.is_action_pressed("ui_down") else false
-	extra_input.Teleport = true if Input.is_action_pressed("ui_space") else false
+	movement_input.Crouch = true if Input.is_action_pressed("ui_down") else false
+	skill_input.Teleport = true if Input.is_action_pressed("ui_space") else false
 
 	# Processes Input and changes motion.x accordingly
 	if x_input != 0:
@@ -64,21 +72,21 @@ func _physics_process(delta):
 	motion = move_and_slide(motion, Vector2.UP)
 
 	# ANIMATION
-	c_animations(x_input, motion.y, extra_input)
+	c_animations(x_input, motion.y, movement_input)
 
 	# VAR ADJUSTMENTS
-	c_adjust_movespeed(extra_input)
+	c_adjust_movespeed(movement_input)
 
 	# SKILLS
-	c_skills(x_input, motion.y, extra_input)
+	c_skills(x_input, motion.y, skill_input)
 
-func c_animations(x, y, eI) -> void:
+func c_animations(x, y, mI) -> void:
 	var idle
 	var move
 	basic_animation_ignore = true if skill == true else false
 
 	if basic_animation_ignore == false:
-		if eI.Crouch == true:
+		if mI.Crouch == true:
 			idle = "CrouchIdle"
 			move = "CrouchMove"
 		else:
@@ -98,23 +106,28 @@ func c_animations(x, y, eI) -> void:
 			spr_player.flip_h = x < 0
 
 
-func c_adjust_movespeed(eI) -> void:
-	MOVE_ADJUST = -32 if eI.Crouch == true and is_on_floor() else 0
+func c_adjust_movespeed(mI) -> void:
+	MOVE_ADJUST = -32 if mI.Crouch == true and is_on_floor() else 0
 		
-func c_skills(x, y, eI) -> void:
+func c_skills(x, y, sI) -> void:
 	# This area is hard coded; Needs better implementation
-	if eI.Teleport == true:
+	if sI.Teleport == true:
 		if x == 0 and y == 0:
+			# Teleport 
 			bullet_time_filter.visible = true
 			error_filter.visible = false
-			pass 
+			self.position = click_pos
+			
 		else:
+			# Teleport error
 			error_filter.visible = true
 			bullet_time_filter.visible = false
 			pass
-	elif eI.Teleport == false:
+	elif sI.Teleport == false:
+		# Either go teleport or cancel
 		bullet_time_filter.visible = false
 		error_filter.visible = false
+		
 	
 	# if eI.Teleport == true:
 	# 	spr_player.play("SkillTeleA")
@@ -127,7 +140,7 @@ func c_skills(x, y, eI) -> void:
 
 
 func _on_spr_player_animation_finished() -> void:
-	if extra_input.Teleport == false and skill == true:
+	if skill_input.Teleport == false and skill == true:
 		skill = false
 		# gravity_ignore = false
 		$cam_player.smoothing_speed = 1
